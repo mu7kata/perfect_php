4579f02cbd1e44f5a71d6f07c48b6289c8096eaa
mu7kata <mu7kata@icloud.com>
2021-01-04 22:45:08 +0900

    second_commit

--- hello.php
+++ hello.php
@@ -16,21 +16,14 @@ require('function.php');
    $red='百田夏菜子';
    $pink='佐々木彩夏';
    $purple='高城れに';
-   $overview=<<<EOI
-   ももいろクローバーZは "$red"・"$yellow"・"$pink"・$purple からなる4人組ガールズユニットである。
-   2014年には、国立競技場でのライブを女性グループとしては初めて行い、2日間で11万人を動員した。
-   ライブの年間動員数においては、過去2度にわたり女性アーティスト1位を記録。
-   EOI;
- 
-   $momoclo=array("$yellow",'$pink',"${red}","$purple");
+   $momoclo= array("$yellow","$pink","${red}","$purple");
 
-   foreach($momoclo as $momoclo_member){
-     echo $momoclo_member;
-     echo '<br>';
-    }
-  echo $overview;
-  
+function add($v1,$v2){
+  return $v1+$v2;
+}
+echo call_user_func('add',1,2);
     
+
       ?></p>
 </body>
 
