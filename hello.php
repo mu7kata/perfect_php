86d6d5a1b5c45df96ca224c90423217221e6325e
mu7kata <mu7kata@icloud.com>
2021-01-05 23:33:36 +0900

    classについて

--- hello.php
+++ hello.php
@@ -18,10 +18,49 @@ require('function.php');
    $purple='高城れに';
    $momoclo= array("$yellow","$pink","${red}","$purple");
 
-function add($v1,$v2){
-  return $v1+$v2;
+function add(&$v1,$v2){
+  echo  pow($v1,$v2);
 }
-echo call_user_func('add',1,2);
+
+$my_pow=function($times=2)
+{
+  return function ($v)use ($times) {
+    return pow($v,$times);
+  };
+};
+
+
+  class Momoclo{
+    public $yellow='玉井詩織';
+    public $red='百田夏菜子';
+    public $pink='佐々木彩夏';
+    public $purple='高城れに';
+
+    public static $miryoku = '元気がいい';
+
+    public function work($v){
+        switch ($this->pink){
+          case '佐々木彩夏':
+            echo 'ぷにっぷに';
+        }
+    }
+  } 
+
+  $idol= new Momoclo();
+  
+class StarDust{
+  const aa='aa';
+
+  public $type;
+  public $name;
+  public function __construct($name,$type)
+  {
+    $this->name=$name;
+    $this->type=$type;
+  }
+}
+$ukka=new StarDust('ukka',StarDust::aa);
+echo $ukka->name;
     
 
       ?></p>
