706cec1bb9ddef396a09c49916c0d0ce3d0db87b
mu7kata <mu7kata@icloud.com>
2021-01-06 21:17:29 +0900

    ひとこと掲示板

---
+++ online_bbs/bbs.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ひとこと掲示板</title>
+</head>
+<body>
+  <h1>ひとこと掲示板</h1>
+
+  <form action="bbs.php" method=post>
+  名前：<input type="text" name='name'><br />
+  ひとこと：<input type="text"　name='comment' size="60"><br />
+  <input type="submit" value="送信"><br />
+  </form>
+
+</body>
+</html>
