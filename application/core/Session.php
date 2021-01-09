f98a2043be35a959524f9029a9d764b5af7d532d
mu7kata <mu7kata@icloud.com>
2021-01-09 15:17:12 +0900

    Session.php作成

---
+++ application/core/Session.php
@@ -0,0 +1,62 @@
+<?php
+class Sesstion
+{
+  protected static $sessionStarted = false;
+  protected static $sessionIdRegenerated = false;
+
+
+//インスタンスが生成されたら自動でセッションスタート
+  public function __construct()
+  {
+if(!self::$sessionStarted){
+  session_start();
+self::$sessionStarted =true;
+}
+  }
+  
+  public function set($name,$value)
+  {
+    $_SESSION[$name]=$value;
+  }
+
+  public function get($name,$default = null)
+  {
+    if(isset($_SESSION[$name])){
+      return $_SESSION[$name];
+    }
+    return $default;
+  }
+  public function remove($name)
+  {
+    unset($_SESSION[$name]);
+  }
+
+  public function clear()
+  {
+    $_SESSION = array();
+  }
+
+  //セッションIDの新規発行
+  public function regenerate($destroy=true)
+  {
+    if(!self::$sessionIdRegenerated){
+      session_regenerate_id($destroy);
+      self::$sessionIdRegenerated = true;
+    }
+  }
+
+  //ログイン状態の判定
+  public function setAuthenticated($bool){
+    $this->set('_authticated',(bool)$bool);
+    //セッションID再発行
+    $this->regenerate();
+  }
+
+public function isAuthenticated()
+{
+  return $this->get('_authenticated',false);
+}
+
+}
+
+?>
