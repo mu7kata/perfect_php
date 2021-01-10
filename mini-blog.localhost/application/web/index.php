efa4b134a0e54cc3cfff1a9efbbf216f18e518dc
mu7kata <mu7kata@icloud.com>
2021-01-10 17:14:30 +0900

    first-commit

---
+++ mini-blog.localhost/application/web/index.php
@@ -0,0 +1,16 @@
+<?PHP
+
+require 'mini-blog.localhost/application/bootstap.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  Hello.world
+</body>
+</html>
